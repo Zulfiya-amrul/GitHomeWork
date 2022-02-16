7d990bc81cd70537abaa47e35d254e9800073bc0
zulfiya-amrul <zulfiya.amrul@gmail.com>
2022-02-16 12:44:22 -0700

    1st commit

---
+++ src/gitHomeWork/gitHW.java
@@ -0,0 +1,13 @@
+package gitHomeWork;
+
+public class gitHW {
+
+	public static void main(String[] args) {
+		// create a code that prints your name
+		
+		System.out.println("My name is Zulfiya.");
+
+		
+	}
+
+}
